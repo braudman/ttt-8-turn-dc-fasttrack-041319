5f1d35f4b8b205c2c74d65a096b797c17a253504
Stephen M Braudo <braudman@gmail.com>
2019-04-17 21:08:20 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,57 @@
+# Define display_board that accepts a board and prints
+# out the current state.
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+
+def input_to_index(input)
+  index = (input.to_i) - 1
+  
+end
+
+board = [" "," "," "," "," "," "," "," "," "]
+def move(board, index, char = "X" )
+  board[index] = char
+end
+
+
+
+
+
+# validates if the space is open or taken by an existing "X" or "O"
+#|| board[index] == " " ||
+      #board[index] == "" || board[index] == nil
+def valid_move?(board, index)
+  if (index <=0 && index >=8)
+    return true
+  elsif (board[index] == " " || board[index] == "" || board[index] == nil)
+  
+   return false
+    end
+end
+ 
+
+
+
+
+
+#determines if the postion on the board is taken or not--
+
+def position_taken?(board, index)
+
+  
+  if (board[index] == " " || board[index] == "" || board[index] == nil)
+    puts "Free Space"
+    return false
+  elsif (board[index] == "X" || board[index] == "O")
+    puts "Space Taken. Choose another."
+    return true
+  end
+  
+end
